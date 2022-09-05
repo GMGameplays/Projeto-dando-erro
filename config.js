23202c6c8305a9ceb91e48c805ace4afa08e8284
Gabriel Martins <46766196+GMGameplays@users.noreply.github.com>
2022-09-05 17:24:51 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIpkeG4J7cyaQE7tFfpbVtsXRRZciSWhg",
+  authDomain: "contador-de-historias-dcf95.firebaseapp.com",
+  databaseURL: "https://contador-de-historias-dcf95-default-rtdb.firebaseio.com",
+  projectId: "contador-de-historias-dcf95",
+  storageBucket: "contador-de-historias-dcf95.appspot.com",
+  messagingSenderId: "518959796830",
+  appId: "1:518959796830:web:1660060cf3877b94969acf"
+};
